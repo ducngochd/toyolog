be03ee1e4ae9b23bf96fca7624c60a3a8d50a064
ducngochd <vdngoc.hust@gmail.com>
2018-11-19 11:57:02 +0900

    d

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '133.18.49.173',
+	'hostname' => '133.18.49.173/phpmyadmin',
 	'username' => 'root',
 	'password' => 'Vdngoc@92',
 	'database' => 'japan_film',
